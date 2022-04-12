9415fa1052b5fdb4b92d85b8073a48c993d99421
miguelhl <mi.hu.li.12@gmail.com>
2022-04-12 11:42:55 -0400

    Add design, provider and basic views of PageHome

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:qr_reader/pages/direcciones_page.dart';
+import 'package:qr_reader/pages/mapas_page.dart';
+import 'package:qr_reader/providers/ui_provider.dart';
 import 'package:qr_reader/widgets/custom_navigator.dart';
 import 'package:qr_reader/widgets/scan_button.dart';
 
@@ -15,12 +19,34 @@ class HomePage extends StatelessWidget {
           IconButton(onPressed: (){}, icon: const Icon(Icons.delete))
         ],
       ),
-      body: const Center(
-        child: Text('Home page'),
-      ),
+      body:  _HomePageBody(),
       bottomNavigationBar: const CustomNavigationBar(),
       floatingActionButton: const  ScanButton(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
+  }
+}
+
+class _HomePageBody extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    //capture the selected menu opt
+
+    final uiProvider= Provider.of<UiProvider>(context);
+
+    final currentIndex = uiProvider.selectedMenuOpt;
+    switch(currentIndex){
+
+      case 0:
+        return const MapasPage();
+      case 1:
+        return const DireccionesPage();
+
+      default:
+        return const MapasPage();
+
+    }
   }
 }
